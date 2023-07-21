fb155fe755b3088a298428ae10a7e0a9051495ae
salceslav <138330878+salceslav@users.noreply.github.com>
2023-07-21 19:10:14 +0300

    fsaf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,5 +12,20 @@ public class Main {
 
             System.out.println("i = " + i);
         }
-    }
+        System.out.printf("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+        System.out.printf("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+     }
 }
